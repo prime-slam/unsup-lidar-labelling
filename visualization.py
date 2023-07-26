41dd8da231ccbb1f7d72ebfca87bce2506647ccb
Artem <amarskiywork@gmail.com>
2023-07-26 19:57:45 +0300

    update default params in visualization.py

--- visualization.py
+++ visualization.py
@@ -39,21 +39,21 @@ def show_colorized_3d_point_cloud(dataset, index_of_image, cam_number):
     )
 
 
-def visualize_3d_point_cloud_on_plot(points):
+def visualize_3d_point_cloud_on_plot(points, subplot_position=111, figsize=(32, 32)):
     x = np.asarray(points)[:, 0]
     y = np.asarray(points)[:, 1]
     z = np.asarray(points)[:, 2]
-    fig = plt.figure(figsize=(32, 32))
-    ax = fig.add_subplot(111, projection="3d")
+    fig = plt.figure(figsize)
+    ax = fig.add_subplot(subplot_position, projection="3d")
     ax.scatter(x, y, z)
     plt.show()
 
 
-def visualize_2d_point_cloud_on_plot(points, image):
+def visualize_2d_point_cloud_on_plot(points, image, point_square = 2, transparency=0.5):
     x = points[0]
     y = points[1]
     depth = points[2]
-    sns.scatterplot(x=x, y=y, hue=depth, alpha=0.5, s=2, legend=False)
+    sns.scatterplot(x=x, y=y, hue=depth, alpha=transparency, s=point_square, legend=False)
     ax = plt.gca()
     ax.set_ylim(ax.get_ylim()[::-1])
     ax.xaxis.tick_top()
@@ -64,12 +64,12 @@ def visualize_2d_point_cloud_on_plot(points, image):
     plt.show()
 
 
-def visualize_2d_colorized_point_cloud_on_plot(points, image):
+def visualize_2d_colorized_point_cloud_on_plot(points, image, point_square = 2, transparency=0.5):
     colors = colorized_point_cloud.get_list_of_point_colors_from_image(points, image)
     x = points[0]
     y = points[1]
     fig, ax = plt.subplots()
-    sns.scatterplot(x=x, y=y, color=colors, alpha=1, s=2, legend=False, ax=ax)
+    sns.scatterplot(x=x, y=y, color=colors, alpha=transparency, s=point_square, legend=False, ax=ax)
     ax.set_aspect("equal")
     ax.set_ylim(ax.get_ylim()[::-1])
     ax.xaxis.tick_top()
